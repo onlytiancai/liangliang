91bbade032613dbcdbdcee1216142b9c88faf997
onlytiancai <onlytiancai@gmail.com>
2012-09-21 17:38:17 +0800

    first commit

---
+++ src/static/liangliang.js
@@ -0,0 +1,75 @@
+var liangliang = liangliang || {};
+
+(function(){
+    var await_timeout = eval(Wind.compile("async", function (task, timeout) {
+        var any = $await(Wind.Async.Task.whenAny( task, Wind.Async.sleep(timeout)));
+        if (any.key != 0) {
+            throw {message:'timeout'};
+        }
+        return any.task.result;
+    }));
+
+    var tests = [];
+
+    var define_test = function(test_name, url, suite, timeout){
+        var test = {
+            name: test_name,
+            url: url,
+            suite: suite ? suite : 'default suite',
+            timeout: timeout ? timeout : 3000,
+            action: function(fun){
+                this.test_action = fun;
+                return this;
+            }
+        };
+        tests.push(test);
+        return test;
+    };
+
+    var TestTask = eval(Wind.compile("async", function(test) {
+        var frm = $('#frm_test'), html = '', result = null;
+
+        var task_wraper = Wind.Async.Task.create(function (t){
+            var win = frm[0].contentWindow;
+            var context = {
+                ok: function(){
+                    t.complete("success");
+                },
+                failure: function(msg){
+                    t.complete("failure", msg);
+                }
+            }
+                
+            test.test_action(win, context);
+        });
+        
+        frm.attr('src', test.url);
+        $await(Wind.Async.onEvent(frm[0], "load"));
+        try{
+            result = $await(await_timeout(task_wraper, test.timeout));
+            html = test.suite + '(' + test.name + '):' + (result == undefined? 'ok': result) + '\r\n';
+        }
+        catch(e){
+            html = test.suite + '(' + test.name + ')error:' + e.message + '\r\n';
+        }
+
+        $('#result').append(html);
+    }));
+
+
+    var run = function(){
+        var AllTaskAsync = eval(Wind.compile("async", function() {
+            for (var i in tests){
+                var test_case = tests[i];
+                $await(TestTask(test_case));
+            }
+        }));
+
+        AllTaskAsync().start();
+    };
+
+    liangliang.await_timeout = await_timeout;
+    liangliang.tests = tests;
+    liangliang.define_test = define_test;
+    liangliang.run = run;
+})();
---
+++ src/test_cases/testcase1.js
@@ -0,0 +1,75 @@
+(function(){
+    var define_test = liangliang.define_test;
+
+    define_test('simple test', 'test_cases/betest.html')
+    .action(function(win, t) {
+        var $ = win.$;
+
+        var check_result = function(){
+            var p_text = win.$('p').text();
+            if (p_text === 'haha'){
+                t.ok();
+            }else{
+                $('p').one("DOMSubtreeModified", check_result);
+            }
+        };
+
+        var test_action = function(){
+            $('button').click();
+        };
+
+        $('p').one("DOMSubtreeModified", check_result);
+        test_action();
+    });
+
+    define_test('add todo item', 'test_cases/backbone_todo.html', 'todo')
+    .action(function(win, t) {
+        var $ = win.$;
+
+        var check_result = function(){
+            var last_todo = $('#todo-list .view:last label');
+            var txt = last_todo.text();
+            if(txt == 'aaaa'){
+                t.ok(); 
+            }
+        };
+
+        var trigger_keypress_enter = function(dom){
+            var e = win.jQuery.Event("keypress");
+            e.which = 13;
+            e.keyCode = 13;
+            dom.trigger(e);
+        };
+
+        var test_action = function(){
+            var new_todo = win.$('#new-todo');
+            new_todo.val('aaaa');
+            trigger_keypress_enter(new_todo);
+        };
+
+        $('#todo-list').one("DOMSubtreeModified", check_result);
+        test_action();
+    });
+
+    define_test('remove todo item', 'test_cases/backbone_todo.html', 'todo')
+    .action(function(win, t) {
+        var $ = win.$, that = this;
+        var todos = win.$('#todo-list .view'), len_todo = todos.length;
+
+        var check_result = function(){
+            var todos = win.$('#todo-list .view');
+            if (todos.length == len_todo - 1){
+                t.ok();
+            }
+        };
+
+        var test_action = function(){
+            var last_todo = win.$('#todo-list .view:last .destroy');
+            last_todo.click();
+        };
+
+        $('#todo-list').one("DOMSubtreeModified", check_result);
+        test_action();
+
+    });
+})();
